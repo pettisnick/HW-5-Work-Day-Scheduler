bc09c9a9e3fd144f8bd733c33c6906b22f4adbad
Nick Pettis <pettisnick@gmail.com>
2020-07-14 18:06:14 -0400

    links

---
+++ javascript.js
@@ -0,0 +1,6 @@
+$("#currentDay").text(moment).format("MMM Do YYY, h:mm:ss a");
+
+<script src="moment.js"></script>
+<script>
+    $("#currentDay").text(moment).format("MMM Do YYYY, h:mm:ss a");
+</script>
